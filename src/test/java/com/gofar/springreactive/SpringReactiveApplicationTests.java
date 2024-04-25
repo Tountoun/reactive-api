b48fbaf6e1722947d7897103f21a9fb5b24e8843
Tountoun <tountounabela@gmail.com>
2024-04-25 04:06:09 +0000

    Project initialization

---
+++ src/test/java/com/gofar/springreactive/SpringReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gofar.springreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
